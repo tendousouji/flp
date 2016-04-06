75c68661fd3dcee6bc775498082cd45e4644b65e
NguyenQuoc <nguyenminhqoc@gmail.com>
2016-04-06 14:36:53 +0700

    repair show posts in admin

--- public/javascripts/admin/controllers/posts.js
+++ public/javascripts/admin/controllers/posts.js
@@ -50,9 +50,12 @@
       MainService.getPosts().then(function(posts){
         // set addtional filed
         posts.forEach(function(post){
-          post.author_name = post.author.username;
-          post.category_name = post.catalory.name;
-          post.type_name = post.type.name;
+          if (post.author == null || post.catalory == null || post.type == null) {
+          } else {
+            post.author_name = post.author.username;
+            post.category_name = post.catalory.name;
+            post.type_name = post.type.name;
+          }
         });
 
         // build ng-table
